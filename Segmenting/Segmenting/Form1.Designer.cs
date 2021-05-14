151f522e9591754be3ab602aa6f3e0a42d02ffd3
VictorPlesko <victorplesko@gmail.com>
2021-05-14 10:32:38 +0300

    Added lab5

---
+++ Segmenting/Segmenting/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Segmenting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(41, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(933, 492);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(982, 249);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Clip";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(37, 514);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(0, 491);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "400";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(16, 15);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(959, 514);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "700";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(492, 514);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 17);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "350";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(0, 249);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 17);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "200";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(0, 130);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 17);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "100";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.Location = new System.Drawing.Point(0, 370);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(32, 17);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "300";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.Color.Black;
+            this.label9.Location = new System.Drawing.Point(264, 514);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(32, 17);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "175";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.ForeColor = System.Drawing.Color.Black;
+            this.label10.Location = new System.Drawing.Point(719, 514);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(32, 17);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "525";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(1020, 213);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(79, 32);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "Tool:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1145, 554);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Plesko Victor, 3c, 12 gr";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ Segmenting/Segmenting/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Segmenting
+{
+    public partial class Form1 : Form
+    {
+        float wxLeft, wxRight, wyBottom, wyTop;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Bitmap bitmap = new Bitmap(700, 400);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            Pen pen = new Pen(ColorTranslator.FromHtml("#87CEEB"), 3);
+
+            string[] lines = File.ReadAllLines("input.txt");
+            string[] rectCoords = lines[lines.Length - 1].Split();
+
+            wxLeft = float.Parse(rectCoords[0]);
+            wyBottom = float.Parse(rectCoords[1]);
+            wxRight = wxLeft + float.Parse(rectCoords[2]);
+            wyTop = wyBottom + float.Parse(rectCoords[3]);
+
+            List<PointF> rectPoly = new List<PointF>();
+            rectPoly.Add(new PointF(wxLeft, wyBottom));
+            rectPoly.Add(new PointF(wxLeft, wyTop));
+            rectPoly.Add(new PointF(wxRight, wyTop));
+            rectPoly.Add(new PointF(wxRight, wyBottom));
+
+            for (int i = 0; i < lines.Length - 1; i++)
+            {
+                string[] coordinates = lines[i].Split();
+
+                if (coordinates.Length == 4)
+                {
+                    PointF p1 = new PointF(int.Parse(coordinates[0]), int.Parse(coordinates[1]));
+                    PointF p2 = new PointF(int.Parse(coordinates[2]), int.Parse(coordinates[3]));
+                    graphics.DrawLine(pen, p1, p2);
+                }
+                else
+                {
+                    List<PointF> polygon = new List<PointF>();
+                    for(int j = 0; j < coordinates.Length; j+=2)
+                    {
+                        PointF p = new PointF(int.Parse(coordinates[j]), int.Parse(coordinates[j+1]));
+                        polygon.Add(p);
+                    }
+                    graphics.DrawPolygon(pen, polygon.ToArray());
+                    graphics.FillPolygon(new SolidBrush(ColorTranslator.FromHtml("#87CEEB")), polygon.ToArray());
+                }
+            }
+
+            pen.Color = ColorTranslator.FromHtml("#008000");
+            graphics.DrawRectangle(pen, wxLeft, wyBottom, float.Parse(rectCoords[2]), float.Parse(rectCoords[3]));
+            pictureBox1.Image = bitmap;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Bitmap bitmap = new Bitmap(700, 400);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            Pen pen = new Pen(ColorTranslator.FromHtml("#87CEEB"), 3);
+
+            string[] lines = File.ReadAllLines("input.txt");
+            string[] rectCoords = lines[lines.Length - 1].Split();
+
+            wxLeft = float.Parse(rectCoords[0]);
+            wyBottom = float.Parse(rectCoords[1]);
+            wxRight = wxLeft + float.Parse(rectCoords[2]);
+            wyTop = wyBottom + float.Parse(rectCoords[3]);
+
+            List<PointF> rectPoly = new List<PointF>();
+            rectPoly.Add(new PointF(wxLeft, wyBottom));
+            rectPoly.Add(new PointF(wxLeft, wyTop));
+            rectPoly.Add(new PointF(wxRight, wyTop));
+            rectPoly.Add(new PointF(wxRight, wyBottom));
+
+            for (int i = 0; i < lines.Length - 1; i++)
+            {
+                string[] coordinates = lines[i].Split();
+
+                if (coordinates.Length == 4)
+                {
+                    PointF p1 = new PointF(int.Parse(coordinates[0]), int.Parse(coordinates[1]));
+                    PointF p2 = new PointF(int.Parse(coordinates[2]), int.Parse(coordinates[3]));
+                    pen.Width = 2;
+                    graphics.DrawLine(pen, p1, p2);
+                    CheckLine(graphics, p1, p2);
+                }
+                else
+                {
+                    List<PointF> polygon = new List<PointF>();
+                    for (int j = 0; j < coordinates.Length; j += 2)
+                    {
+                        PointF p = new PointF(int.Parse(coordinates[j]), int.Parse(coordinates[j + 1]));
+                        polygon.Add(p);
+                    }
+                    graphics.DrawPolygon(pen, polygon.ToArray());
+                    graphics.FillPolygon(new SolidBrush(ColorTranslator.FromHtml("#87CEEB")), polygon.ToArray());
+
+                    CheckPolygon(graphics, polygon.ToArray(), rectPoly.ToArray());
+                }
+            }
+
+            pen.Color = ColorTranslator.FromHtml("#008000");
+            graphics.DrawRectangle(pen, wxLeft, wyBottom, float.Parse(rectCoords[2]), float.Parse(rectCoords[3]));
+            pictureBox1.Image = bitmap;
+        }
+
+        int GetPointCode(PointF point)
+        {
+            int i = 0;
+            if (point.X < wxLeft) i++;
+            else if (point.X > wxRight) i += 2;
+
+            if (point.Y < wyBottom) i += 4;
+            else if (point.Y > wyTop) i += 8;
+
+            return i;
+        }
+
+        void CheckLine(Graphics graphics, PointF p1, PointF p2)
+        {
+            Pen pen = new Pen(ColorTranslator.FromHtml("#008000"), 3);
+
+            int s;
+            int code1 = GetPointCode(p1);
+            int code2 = GetPointCode(p2);
+
+            if (code1 == 0 && code2 == 0)
+            {
+                graphics.DrawLine(pen, p1, p2);
+                return;
+            }
+
+            if ((code1 & code2) == 1)
+                return;
+
+            float dxdy = 0, dydx = 0;
+            float dx = p2.X - p1.X;
+            float dy = p2.Y - p1.Y;
+
+            if (dx != 0)
+            {
+                dydx = dy / dx;
+            }
+            else
+            {
+                if (dy == 0)
+                {
+                    if (code1 == 0 && code2 == 0) {
+                        graphics.DrawLine(pen, p1, p2);
+                        return;
+                    }
+                    else return;
+                }
+            }
+            if (dy != 0)
+            {
+                dxdy = dx / dy;
+            }
+
+            int iter = 4;
+            do
+            {
+                if ((code1 & code2) == 1)
+                    return;
+
+                if (code1 == 0 && code2 == 0)
+                {
+                    graphics.DrawLine(pen, p1, p2);
+                    return;
+                }
+                    
+                if (code1 == 0)
+                { 
+                    s = code1; code1 = code2; code2 = s;
+                    PointF p = p1; p1 = p2; p2 = p;
+                }
+
+                if ((code1 & 1) == 1)
+                {     
+                    p1.Y += dydx * (wxLeft - p1.X);
+                    p1.X = wxLeft;
+                }
+                else if ((code1 & 2) == 2)
+                {
+                    p1.Y += dydx * (wxRight - p1.X);
+                    p1.X = wxRight;
+                }
+                else if ((code1 & 4) == 4)
+                {
+                    p1.X += dxdy * (wyBottom - p1.Y);
+                    p1.Y = wyBottom;
+                }
+                else if ((code1 & 8) == 8)
+                {
+                    p1.X += dxdy * (wyTop - p1.Y);
+                    p1.Y = wyTop;
+                }
+                code1 = GetPointCode(p1);
+            } while (--iter >= 0);
+        }
+
+        void CheckPolygon(Graphics graphics, PointF[] subjectPolygon, PointF[] clipPolygon)
+        {
+            PointF cp1, cp2, s, e;
+
+            bool inside(PointF p)
+            {
+                return (cp2.X - cp1.X) * (p.Y - cp1.Y) < (cp2.Y - cp1.Y) * (p.X - cp1.X);
+            }
+
+            PointF intersection() {
+                float[] dc = { cp1.X - cp2.X, cp1.Y - cp2.Y },
+                    dp = { s.X - e.X, s.Y - e.Y };
+
+                float
+                    n1 = cp1.X * cp2.Y - cp1.Y * cp2.X,
+                    n2 = s.X * e.Y - s.Y * e.X,
+                    n3 = 1.0f / (dc[0] * dp[1] - dc[1] * dp[0]);
+
+                PointF res = new PointF((n1 * dp[0] - n2 * dc[0]) * n3, (n1 * dp[1] - n2 * dc[1]) * n3);
+                return res;
+            };
+
+            List<PointF> outputList = new List<PointF>();
+            foreach (PointF point in subjectPolygon)
+                outputList.Add(point);
+
+            cp1 = clipPolygon[clipPolygon.Length - 1];
+            for (int j = 0; j < clipPolygon.Length; j++)
+            {
+                cp2 = clipPolygon[j];
+                List<PointF> inputList = outputList.ToList();
+                outputList.Clear();
+
+                if (inputList.Count > 0)
+                {
+                    s = inputList[inputList.Count - 1];
+
+                    foreach (PointF item in inputList)
+                    {
+                        e = item;
+                        if (inside(e))
+                        {
+                            if (!inside(s))
+                            {
+                                outputList.Add(intersection());
+                            }
+                            outputList.Add(e);
+                        }
+                        else if (inside(s))
+                        {
+                            outputList.Add(intersection());
+                        }
+                        s = e;
+                    }
+                }
+                cp1 = cp2;
+            }
+
+            if (outputList.Count > 0)
+            {
+                Pen pen = new Pen(ColorTranslator.FromHtml("#008000"), 3);
+                graphics.DrawPolygon(pen, outputList.ToArray());
+                graphics.FillPolygon(new SolidBrush(ColorTranslator.FromHtml("#008000")), outputList.ToArray());
+            }
+        }
+    }
+}
